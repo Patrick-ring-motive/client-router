e9f74ff06fa36c6505a2ceb8aee53cbc461ef155
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-23 20:26:41 -0500

    Create client-router.js

---
+++ client-router.js
@@ -0,0 +1,11 @@
+    (() => {
+      if(typeof env === 'undefined'){
+        globalThis.env = {};
+      }
+      env.hostRewrites = [];
+      env.pathRewrites = [];
+      (()=>{
+        const _fetch = globalThis.fetch;
+        globalThis.fetch = 
+      })();
+  })();
