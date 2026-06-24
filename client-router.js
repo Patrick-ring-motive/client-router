e0302b85daf8e6a8aed4c70be8e6e34065b3afa3
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-24 01:57:54 -0500

    style: js-beautify formatting (client-router.js)

--- client-router.js
+++ client-router.js
@@ -1,13 +1,13 @@
     (() => {
-      if(typeof env === 'undefined'){
+      if (typeof env === 'undefined') {
         globalThis.env = {};
       }
       env.hostRewrites = [];
       env.pathRewrites = [];
-      (()=>{
+      (() => {
         const _fetch = globalThis.fetch;
-        globalThis.fetch = Object.setPrototypeOf(async function fetch(...args){
-            return fetch.apply(this,args);
-        },_fetch);
+        globalThis.fetch = Object.setPrototypeOf(async function fetch(...args) {
+          return fetch.apply(this, args);
+        }, _fetch);
       })();
-  })();
+    })();
