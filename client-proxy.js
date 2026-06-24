dc3ce803231251fe8b17396f1e358508816cce37
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-24 01:57:57 -0500

    style: js-beautify formatting (client-proxy.js)

--- client-proxy.js
+++ client-proxy.js
@@ -1,17 +1,17 @@
     (() => {
-      (()=>{
+      (() => {
         const _fetch = globalThis.fetch;
-        globalThis.fetch = Object.setPrototypeOf(async function fetch(...args){
-            const url = String(args[0].url ?? args[0]);
-            const host = new URL(url).host;
-            if(location.host !== host){
-                args[1] ??= Object(args[0]);
-                const proxy = new URL(location.origin);
-                proxy.pathname = 'proxy';
-                proxy.searchParams.set('url',url);
-                args[0] = String(proxy);
-            }
-            return fetch.apply(this,args);
-        },_fetch);
+        globalThis.fetch = Object.setPrototypeOf(async function fetch(...args) {
+          const url = String(args[0].url ?? args[0]);
+          const host = new URL(url).host;
+          if (location.host !== host) {
+            args[1] ??= Object(args[0]);
+            const proxy = new URL(location.origin);
+            proxy.pathname = 'proxy';
+            proxy.searchParams.set('url', url);
+            args[0] = String(proxy);
+          }
+          return fetch.apply(this, args);
+        }, _fetch);
       })();
-  })();
+    })();
